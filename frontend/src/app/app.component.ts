3497975f031fadda4b05d7ad433feaebd239deb8
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2023-11-06 19:17:53 +0100

    Use App as wallpaper container.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject} from '@angular/core';
+import {WallpaperService} from "./service/wallpaper.service";
+import {Some} from "option-value";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'frontend';
-  something: string = "hello :)"
+
+  host = inject(ElementRef)
+  wallpaperService = inject(WallpaperService)
+
+  ngAfterViewInit(): void {
+    this.wallpaperService.backgroundElement = Some(this.host.nativeElement as HTMLDivElement)
+  }
 }
--- frontend/src/app/main-screen/main-screen.component.ts
+++ frontend/src/app/main-screen/main-screen.component.ts
@@ -2,15 +2,6 @@ import {AfterViewInit, Component, inject, ViewChild, ViewContainerRef} from '@an
 import {NotificationService} from "../service/notification.service";
 import {WindowService} from "../service/window.service";
 import {WallpaperService} from "../service/wallpaper.service";
-import {
-  AdminActivationEmailEditorComponent
-} from "../admin/admin-activation-email-editor/admin-activation-email-editor.component";
-import {W2kWindowFrameComponent} from "../ui-elements/w2k-window-frame/w2k-window-frame.component";
-import {ChangePasswordComponent} from "../apps/change-password/change-password.component";
-import {AdminManageDomainsComponent} from "../admin/admin-manage-domains/admin-manage-domains.component";
-import {UserProfileComponent} from "../apps/user-profile/user-profile.component";
-import {ChangeProfilePictureComponent} from "../apps/change-profile-picture/change-profile-picture.component";
-import {DomainService} from "../service/domain.service";
 
 @Component({
   selector: 'app-main-screen',
